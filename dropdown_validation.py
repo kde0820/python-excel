48ae52f52a91f0c81e06589c9a294570a32c587d
kde0820 <kde0820@naver.com>
2018-12-20 18:00:59 +0900

    add set_column and hide sheet2

--- dropdown_validation.py
+++ dropdown_validation.py
@@ -3,14 +3,17 @@ import xlsxwriter
 data_list = ['A+', 'A', 'B+', 'B', 'C+', 'C', 'F']
 workbook = xlsxwriter.Workbook('dropdown_test.xlsx')
 worksheet = workbook.add_worksheet()
+worksheet2 = workbook.add_worksheet()
 
+worksheet2.write_column('A1', data_list)
+worksheet2.hide()
+
+worksheet.set_column('A:A', 15)
 worksheet.write_row('A1', ['과목', '등급'])
-
-worksheet.write_column('D2', data_list)
 
 worksheet.write('A2', '데이터 베이스')
 worksheet.data_validation('B2', {'validate': 'list',
-                                 'source': '=$D$2:$D$9'})
+                                 'source': '=Sheet2!$A$1:$A$7'})
 
 worksheet.write('A3', '소프트웨어 공학')
 worksheet.data_validation('B3', {'validate': 'list',
